a44d153d32da2077ab1115c06591cb097852b2c4
Donika <marchevadonika@gmail.com>
2024-01-14 18:03:59 +0200

    update with problem 2

--- 01.Exercise Data Definition and Datatypes/01.Create Tables.sql
+++ 01.Exercise Data Definition and Datatypes/01.Create Tables.sql
@@ -13,4 +13,12 @@ CREATE TABLE towns (
     
     constraint pk_town_id
     PRIMARY KEY (town_id)
-)
+);
+
+USE minions;
+
+ALTER TABLE minions
+ADD COLUMN town_id int;
+
+ALTER TABLE minions
+ADD constraint fk_town_id FOREIGN KEY (town_id) references towns(id);
