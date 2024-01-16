e80d5e808fcb3ae08230bdece2adde79f300ef4b
Donika <marchevadonika@gmail.com>
2024-01-16 15:42:06 +0200

    problem 10 with solution

--- 02.Exercises Basic CRUD/02.Basic-CRUD-Exercises.sql
+++ 02.Exercises Basic CRUD/02.Basic-CRUD-Exercises.sql
@@ -1,6 +1,9 @@
 use soft_uni;
 
-SELECT * FROM departments
+SELECT 
+    *
+FROM
+    departments
 ORDER BY department_id;
 
 SELECT 
@@ -9,26 +12,63 @@ FROM
     departments
 ORDER BY department_id;
 
-SELECT first_name, last_name, salary FROM employees
-ORDER BY employee_id;
-
-SELECT first_name, middle_name, last_name FROM employees
+SELECT 
+    first_name, last_name, salary
+FROM
+    employees
 ORDER BY employee_id;
 
 SELECT 
-    CONCAT(first_name, '.', last_name, '@softuni.bg') AS 'full_ email_address'
+    first_name, middle_name, last_name
+FROM
+    employees
+ORDER BY employee_id;
+
+SELECT 
+    CONCAT(first_name,
+            '.',
+            last_name,
+            '@softuni.bg') AS 'full_ email_address'
 FROM
     employees;
 
-SELECT DISTINCT salary FROM employees;
+SELECT DISTINCT
+    salary
+FROM
+    employees;
 
-SELECT * FROM employees WHERE job_title='Sales Representative'
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    job_title = 'Sales Representative'
 ORDER BY employee_id;
 
-SELECT first_name, last_name, job_title FROM employees
-WHERE salary BETWEEN 20000 AND 30000
+SELECT 
+    first_name, last_name, job_title
+FROM
+    employees
+WHERE
+    salary BETWEEN 20000 AND 30000
 ORDER BY employee_id;
 
-SELECT
-CONCAT_WS(first_name, middle_name, last_name) AS 'Full name'
-WHERE salary=25000 OR  salary=14000 OR salary=12500 OR salary=23600;
+SELECT 
+    CONCAT(first_name,
+            ' ',
+            middle_name,
+            ' ',
+            last_name) AS 'Full name'
+FROM
+    employees
+WHERE
+    salary = 25000 OR salary = 14000
+        OR salary = 12500
+        OR salary = 23600;
+        
+SELECT 
+    first_name, last_name
+FROM
+    employees
+WHERE
+    manager_id IS NULL;
