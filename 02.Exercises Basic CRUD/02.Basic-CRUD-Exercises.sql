356c8dbedc520a140faab95fc6e5b5e6a8b41179
Donika <marchevadonika@gmail.com>
2024-01-17 08:08:29 +0200

    17

--- 02.Exercises Basic CRUD/02.Basic-CRUD-Exercises.sql
+++ 02.Exercises Basic CRUD/02.Basic-CRUD-Exercises.sql
@@ -104,6 +104,14 @@ CREATE VIEW v_employees_salaries AS
 SELECT first_name, last_name, salary
 FROM employees;
 
+CREATE VIEW v_employees_job_titles  AS
+SELECT CONCAT(first_name, ' ', IF(middle_name IS NULL, '',CONCAT(middle_name, ' ')), last_name) AS full_name, job_title
+FROM employees;
+
+SELECT * FROM v_employees_job_titles;
+
+SELECT DISTINCT job_title FROM employees
+ORDER BY job_title ASC;
 
 
 
