39fceed2760f138b02865718a7d3c34d8569255d
Donika <marchevadonika@gmail.com>
2024-01-18 15:14:51 +0200

    correct solution of 23 and add solution of problem 24
    correction is for the name of the currency

--- 02.Exercises Basic CRUD/02.Basic-CRUD-Exercises.sql
+++ 02.Exercises Basic CRUD/02.Basic-CRUD-Exercises.sql
@@ -140,14 +140,18 @@ WHERE continent_code='EU'
 ORDER BY population DESC, country_name
 LIMIT 30;
 
+use geography;
+
 SELECT 
     country_name,
     country_code,
     IF(currency_code != 'EUR',
         'Not Euro',
-        'Euro')
+        'Euro') AS currency
 FROM
     countries
 ORDER BY country_name;
 
+SELECT name FROM characters
+ORDER BY name;
 
