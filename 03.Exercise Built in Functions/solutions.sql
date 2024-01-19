0cd920842fa20069bf00b86fd4c65fba01105d7f
Donika <marchevadonika@gmail.com>
2024-01-19 22:58:17 +0200

    3

--- 03.Exercise Built in Functions/solutions.sql
+++ 03.Exercise Built in Functions/solutions.sql
@@ -8,3 +8,8 @@ FROM employees
 WHERE last_name LIKE '%ei%'
 ORDER BY employee_id;
 
+SELECT first_name
+FROM employees
+WHERE department_id IN(3,10) AND
+YEAR(hire_date) BETWEEN 1995 AND 2005
+ORDER BY employee_id;
