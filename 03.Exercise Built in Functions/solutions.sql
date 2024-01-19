6d31a97448d48fa573553c798e1abe48543b8081
Donika <marchevadonika@gmail.com>
2024-01-19 22:46:54 +0200

    2

--- 03.Exercise Built in Functions/solutions.sql
+++ 03.Exercise Built in Functions/solutions.sql
@@ -2,3 +2,9 @@ SELECT first_name, last_name
 FROM employees
 WHERE first_name LIKE 'Sa%'
 ORDER BY employee_id;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%ei%'
+ORDER BY employee_id;
+
