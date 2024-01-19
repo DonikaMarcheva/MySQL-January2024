1e8bfc5da9d9f4e5c91017c29708af70fe5f5eb2
Donika <marchevadonika@gmail.com>
2024-01-19 23:05:42 +0200

    4

--- 03.Exercise Built in Functions/solutions.sql
+++ 03.Exercise Built in Functions/solutions.sql
@@ -13,3 +13,8 @@ FROM employees
 WHERE department_id IN(3,10) AND
 YEAR(hire_date) BETWEEN 1995 AND 2005
 ORDER BY employee_id;
+
+SELECT first_name, last_name
+FROM employees
+WHERE job_title NOT LIKE '%engineer%'
+ORDER BY employee_id;
