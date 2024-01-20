23bd7ce81e9168d27497ebafb52a6980604faf9f
Donika <marchevadonika@gmail.com>
2024-01-20 15:19:26 +0200

    8

--- 03.Exercise Built in Functions/solutions.sql
+++ 03.Exercise Built in Functions/solutions.sql
@@ -45,3 +45,11 @@ SELECT *
 FROM towns
 WHERE left(name,1) NOT IN ('R','B','D')
 ORDER BY name;
+
+
+CREATE VIEW v_employees_hired_after_2000 AS
+SELECT first_name, last_name 
+FROM employees
+WHERE year(hire_date)>2000;
+
+SELECT * FROM v_employees_hired_after_2000;
