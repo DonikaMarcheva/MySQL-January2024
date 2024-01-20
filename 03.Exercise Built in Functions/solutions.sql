cfb35b5b2d22d90903e26b3e410ca45dcec77b82
Donika <marchevadonika@gmail.com>
2024-01-20 20:23:11 +0200

    15

--- 03.Exercise Built in Functions/solutions.sql
+++ 03.Exercise Built in Functions/solutions.sql
@@ -86,4 +86,23 @@ ORDER BY `email provider`, user_name;
 SELECT user_name, ip_address
 FROM users
 WHERE ip_address LIKE "___.1%.%.___"
-ORDER BY user_name
+ORDER BY user_name;
+
+SELECT name AS game, 
+CASE
+WHEN HOUR(start) < 12 THEN 'Morning'
+WHEN HOUR(start) < 18 THEN 'Afternoon'
+WHEN HOUR(start) < 24 THEN 'Evening'
+END
+AS `Part of the Day`, 
+
+CASE
+WHEN duration <=3 THEN 'Extra Short'
+WHEN duration <=6 THEN 'Short'
+WHEN duration <=10 THEN 'Long'
+ELSE 'Extra Long'
+END
+AS `Duration`
+FROM games
+
+
