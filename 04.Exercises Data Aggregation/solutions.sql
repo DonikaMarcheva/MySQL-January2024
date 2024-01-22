7a278b571f94044b0d948e65eb2d335597aafc54
Donika <marchevadonika@gmail.com>
2024-01-22 16:22:37 +0200

    5

--- 04.Exercises Data Aggregation/solutions.sql
+++ 04.Exercises Data Aggregation/solutions.sql
@@ -19,3 +19,9 @@ FROM wizzard_deposits
 GROUP BY deposit_group
 ORDER BY AVG(magic_wand_size)
 LIMIT 1;
+
+#5
+SELECT deposit_group, SUM(deposit_amount) AS total_sum
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY total_sum;
