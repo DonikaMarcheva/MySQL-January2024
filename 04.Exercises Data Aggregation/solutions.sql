b4817d501677fe3ad3397c5e93dc66101ba17ec9
Donika <marchevadonika@gmail.com>
2024-01-22 16:11:01 +0200

    3

--- 04.Exercises Data Aggregation/solutions.sql
+++ 04.Exercises Data Aggregation/solutions.sql
@@ -6,3 +6,9 @@ FROM wizzard_deposits;
 #2
 SELECT MAX(magic_wand_size) AS 'longest_magic_wand'
 FROM wizzard_deposits;
+
+#3
+SELECT deposit_group, MAX(magic_wand_size) AS 'longest_magic_wand'
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY `longest_magic_wand`, deposit_group;
