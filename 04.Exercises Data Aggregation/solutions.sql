446681f9e7375316658b5bff089b745a04571baf
Donika <marchevadonika@gmail.com>
2024-01-23 12:46:50 +0200

    12

--- 04.Exercises Data Aggregation/solutions.sql
+++ 04.Exercises Data Aggregation/solutions.sql
@@ -71,4 +71,14 @@ WHERE deposit_group='Troll Chest'
 GROUP BY first_letter
 ORDER BY first_letter;
 
+#11
+
+#12
+SELECT department_id, MIN(salary) AS minimum_salary
+FROM employees
+WHERE department_id IN (2,5,7) AND DATE(hire_date) > 01-01-2000
+GROUP BY department_id;
+
+
+
 
