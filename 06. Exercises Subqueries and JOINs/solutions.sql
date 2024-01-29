66696e3c7ae3cc434406403bcc4f9e4d717a0f2e
Donika <marchevadonika@gmail.com>
2024-01-29 20:20:58 +0200

    4

--- 06. Exercises Subqueries and JOINs/solutions.sql
+++ 06. Exercises Subqueries and JOINs/solutions.sql
@@ -32,3 +32,18 @@ FROM
 WHERE
     d.name = 'Sales'
 ORDER BY e.employee_id DESC;
+
+#4
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.salary,
+    d.name AS department_name
+FROM
+    employees AS e
+        JOIN
+    departments AS d ON d.department_id = e.department_id
+WHERE
+    e.salary > 15000
+ORDER BY d.department_id DESC
+LIMIT 5;
