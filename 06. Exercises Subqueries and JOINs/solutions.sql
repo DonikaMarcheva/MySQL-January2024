231cf6c050df62320ee73c36dde489189f5b31fe
Donika <marchevadonika@gmail.com>
2024-01-29 19:38:09 +0200

    2

--- 06. Exercises Subqueries and JOINs/solutions.sql
+++ 06. Exercises Subqueries and JOINs/solutions.sql
@@ -1,6 +1,20 @@
-#1
-SELECT e.employee_id, e.job_title, e.address_id, a.address_text
-FROM employees AS e
-JOIN addresses AS a ON a.address_id=e.address_id
+SELECT 
+    e.employee_id, e.job_title, e.address_id, a.address_text
+FROM
+    employees AS e
+        JOIN
+    addresses AS a ON a.address_id = e.address_id
 ORDER BY address_id
+LIMIT 5;
+
+#2
+SELECT 
+    e.first_name, e.last_name, t.name AS town, a.address_text
+FROM
+    employees AS e
+        JOIN
+    addresses AS a ON a.address_id = e.address_id
+        JOIN
+    towns AS t ON t.town_id = a.town_id
+ORDER BY e.first_name , e.last_name
 LIMIT 5;
