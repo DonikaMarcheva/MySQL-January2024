4b583d1f9a83508beb9023c9b40f8b08316fa77e
Donika <marchevadonika@gmail.com>
2024-01-31 22:22:51 +0200

    17

--- 06. Exercises Subqueries and JOINs/solutions.sql
+++ 06. Exercises Subqueries and JOINs/solutions.sql
@@ -207,6 +207,46 @@ FROM mountains_countries mc
 RIGHT JOIN countries c ON c.country_code= mc.country_code
 WHERE mc.mountain_id IS NULL;
 
+#17
+#first solution
+SELECT
+    c.country_name,
+    MAX(p.elevation) AS 'highest_peak_elevation',
+    MAX(r.length) AS 'longest_river_length'
+FROM
+    countries c
+    LEFT JOIN mountains_countries mc USING (country_code)
+    LEFT JOIN peaks p USING (mountain_id)
+    LEFT JOIN countries_rivers cr USING (country_code)
+    LEFT JOIN rivers r ON cr.river_id = r.id
+GROUP BY
+    c.country_name
+ORDER BY
+    highest_peak_elevation DESC,
+    longest_river_length DESC,
+    c.country_name
+LIMIT
+    5;
+
+#second solution
+SELECT
+    c.country_name,
+    MAX(p.elevation) AS 'highest_peak_elevation',
+    MAX(r.length) AS 'longest_river_length'
+FROM
+    countries c
+    LEFT JOIN mountains_countries mc USING (country_code)
+    LEFT JOIN peaks p USING (mountain_id)
+    LEFT JOIN countries_rivers cr USING (country_code)
+    LEFT JOIN rivers r ON cr.river_id = r.id
+GROUP BY
+    c.country_name
+ORDER BY
+    highest_peak_elevation DESC,
+    longest_river_length DESC,
+    c.country_name
+LIMIT
+    5;
 
 
 
