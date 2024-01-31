e560a83d74aa5ee57d9008408fd72ac023c3014c
Donika <marchevadonika@gmail.com>
2024-01-31 15:33:19 +0200

    10

--- 06. Exercises Subqueries and JOINs/solutions.sql
+++ 06. Exercises Subqueries and JOINs/solutions.sql
@@ -128,6 +128,22 @@ WHERE
     manager_id IN (3 , 7)
 ORDER BY first_name;
 
+#10
+SELECT 
+    e.employee_id,
+    CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
+    CONCAT(e1.first_name, ' ', e1.last_name) AS manager_name,
+    d.name AS department_name
+FROM
+    employees AS e
+        JOIN
+    employees AS e1 ON e1.employee_id = e.manager_id
+        JOIN
+    departments AS d ON d.department_id = e.department_id
+ORDER BY employee_id
+LIMIT 5;
+   
+
 
 
 
