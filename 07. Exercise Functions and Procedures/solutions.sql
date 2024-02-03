b8e0278ff943e918bf8456da81f328c751e3c10e
Donika <marchevadonika@gmail.com>
2024-02-03 20:49:17 +0200

    5

--- 07. Exercise Functions and Procedures/solutions.sql
+++ 07. Exercise Functions and Procedures/solutions.sql
@@ -75,3 +75,19 @@ END $
 DELIMITER ;
 
 CALL usp_get_employees_from_town ('Sofia');
+
+#5
+DELIMITER $
+CREATE FUNCTION ufn_get_salary_level(salary DECIMAL(10,4)) RETURNS VARCHAR(10)
+DETERMINISTIC
+BEGIN
+    IF salary < 30000 THEN RETURN 'Low';
+    ELSEIF salary <= 50000 THEN RETURN 'Average';
+    ELSE RETURN 'High';
+    END IF;
+END $
+
+DELIMITER ;
+
+SELECT UFN_GET_SALARY_LEVEL(50000);
+
