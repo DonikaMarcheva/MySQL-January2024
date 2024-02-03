ed6fe706afd0e86454024ac9b40bff9f2296e0df
Donika <marchevadonika@gmail.com>
2024-02-03 20:14:34 +0200

    4

--- 07. Exercise Functions and Procedures/solutions.sql
+++ 07. Exercise Functions and Procedures/solutions.sql
@@ -58,3 +58,20 @@ END $
 DELIMITER ;
 
 CALL usp_get_towns_starting_with  ('b');
+
+#4
+DELIMITER $
+
+CREATE PROCEDURE usp_get_employees_from_town (expected_town_name VARCHAR(30))
+BEGIN
+SELECT e.first_name, e.last_name
+FROM employees e
+JOIN addresses USING(address_id)
+JOIN towns t USING(town_id)
+WHERE t.name= expected_town_name
+ORDER BY e.first_name, e.last_name, e.employee_id;
+END $
+
+DELIMITER ;
+
+CALL usp_get_employees_from_town ('Sofia');
