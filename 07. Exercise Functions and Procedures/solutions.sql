cd3a0e401947fd4148afb70723f58bd17e775b05
Donika <marchevadonika@gmail.com>
2024-02-03 18:55:13 +0200

    #2

--- 07. Exercise Functions and Procedures/solutions.sql
+++ 07. Exercise Functions and Procedures/solutions.sql
@@ -12,3 +12,18 @@ END $
 DELIMITER ;
 
 CALL usp_get_employees_salary_above_35000();
+
+#2
+DELIMITER $
+
+CREATE PROCEDURE usp_get_employees_salary_above(target_salary DECIMAL(10,4))
+BEGIN
+SELECT first_name, last_name FROM employees
+WHERE salary >= target_salary
+ORDER BY first_name, last_name, employee_id;
+
+END $
+
+DELIMITER ;
+
+CALL usp_get_employees_salary_above (45000);
