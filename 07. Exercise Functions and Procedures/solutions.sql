eb06f8630314575b7a7be50c0970e94e1ce1b036
Donika <marchevadonika@gmail.com>
2024-02-04 20:08:30 +0200

    11

--- 07. Exercise Functions and Procedures/solutions.sql
+++ 07. Exercise Functions and Procedures/solutions.sql
@@ -203,6 +203,32 @@ DELIMITER ;
 
 SELECT ufn_calculate_future_value(1000,0.5,5)
 
+#11
+DELIMITER $
+
+CREATE FUNCTION ufn_calculate_future_value (initial DECIMAL(19,4), interest_rate DECIMAL(10,4),
+num_years INT) 
+RETURNS DECIMAL(19,4)
+READS SQL DATA
+BEGIN
+RETURN initial * POW(1+interest_rate, num_years);
+END $
+
+CREATE PROCEDURE usp_calculate_future_value_for_account (acc_id INT, interest DECIMAL(10,4))
+BEGIN
+SELECT a.id AS account_id, 
+ah.first_name, ah.last_name, 
+a.balance AS current_balance,
+ufn_calculate_future_value(a.balance, interest, 5) AS balance_in_5_years
+FROM account_holders ah
+JOIN accounts a ON ah.id= a.account_holder_id
+WHERE a.id=acc_id;
+END$
+
+DELIMITER ;
+
+CALL usp_calculate_future_value_for_account(1,0.1);
+
 
 
 
