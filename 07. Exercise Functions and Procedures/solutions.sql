4887c02061f95633d75a76004fc8b83813eb0dd1
Donika <marchevadonika@gmail.com>
2024-02-05 14:54:28 +0200

    14

--- 07. Exercise Functions and Procedures/solutions.sql
+++ 07. Exercise Functions and Procedures/solutions.sql
@@ -251,14 +251,14 @@ CALL usp_deposit_money(1,10);
 
 #13
 DELIMITER $
-CREATE PROCEDURE `usp_withdraw_money`(`account_id` INT, `money_amount` DECIMAL(19, 4))
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(19, 4))
 BEGIN
-    IF (`money_amount` > 0) THEN
+    IF (money_amount > 0) THEN
         START TRANSACTION;
-        UPDATE `accounts` AS `a`
-        SET `a`.`balance`=`a`.`balance` - `money_amount`
-        WHERE `a`.`id` = `account_id`;
-        IF (SELECT `balance` FROM `accounts` WHERE `id` = `account_id`) < 0
+        UPDATE accounts AS a
+        SET a.balance=a.balance - money_amount
+        WHERE a.id = account_id;
+        IF (SELECT balance FROM accounts WHERE id = account_id) < 0
         THEN
             ROLLBACK ;
         ELSE
@@ -268,7 +268,10 @@ BEGIN
 END $
 DELIMITER ;
 
+SELECT * FROM accounts WHERE id=1;
+CALL usp_withdraw_money(1,210);
 
+#14
 
 
 
