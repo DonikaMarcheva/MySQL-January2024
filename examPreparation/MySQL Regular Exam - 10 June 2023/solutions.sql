34c6b0e15380ac4d3f2bbaff94fb8ac0db3f4a75
Donika <marchevadonika@gmail.com>
2024-02-06 19:36:34 +0200

    7

--- examPreparation/MySQL Regular Exam - 10 June 2023/solutions.sql
+++ examPreparation/MySQL Regular Exam - 10 June 2023/solutions.sql
@@ -101,3 +101,16 @@ WHERE
     age >= 21
 ORDER BY first_name DESC , email , id
 LIMIT 10;
+
+#7
+SELECT 
+    CONCAT(s.first_name, ' ', s.last_name) AS full_name,
+    SUBSTRING(s.email, 2, 10) AS username,
+    REVERSE(s.phone) AS password
+FROM
+    students s
+        LEFT JOIN
+    students_courses sc ON sc.student_id = s.id
+WHERE
+    sc.course_id IS NULL
+ORDER BY password DESC;
