6760c9e398384008fbf2a96ac1482b0307fae60c
Donika <marchevadonika@gmail.com>
2024-02-06 11:53:55 +0200

    2

--- examPreparation/MySQL Regular Exam - 10 June 2023/solutions.sql
+++ examPreparation/MySQL Regular Exam - 10 June 2023/solutions.sql
@@ -59,3 +59,13 @@ FOREIGN KEY (course_id) REFERENCES courses(id)
 );
 
 #2
+INSERT INTO courses(name , duration_hours, start_date, teacher_name, description, university_id)
+SELECT 
+CONCAT(teacher_name, ' course'),
+length(name)/10,
+adddate(start_date, INTERVAL 5 DAY),
+reverse(teacher_name),
+CONCAT('Course ', `teacher_name`, REVERSE(description)),
+DAY(start_date)
+FROM courses
+WHERE id<=5;
