8f3e3cffff88875ae6ac130fd1de23c80613968a
Donika <marchevadonika@gmail.com>
2024-02-06 21:35:16 +0200

    8

--- examPreparation/MySQL Regular Exam - 10 June 2023/solutions.sql
+++ examPreparation/MySQL Regular Exam - 10 June 2023/solutions.sql
@@ -114,3 +114,18 @@ FROM
 WHERE
     sc.course_id IS NULL
 ORDER BY password DESC;
+
+#8
+SELECT 
+    COUNT(sc.student_id) AS students_count,
+    u.name AS university_name
+FROM
+    students_courses sc
+        JOIN
+    courses c ON c.id = sc.course_id
+        JOIN
+    universities u ON u.id = c.university_id
+GROUP BY u.name
+HAVING students_count>=8
+ORDER BY students_count DESC , university_name DESC;
+
