3a8c9a0e3a89a0cf027915667a33a3dd60a28a35
Donika <marchevadonika@gmail.com>
2024-02-06 21:55:33 +0200

    9

--- examPreparation/MySQL Regular Exam - 10 June 2023/solutions.sql
+++ examPreparation/MySQL Regular Exam - 10 June 2023/solutions.sql
@@ -129,3 +129,22 @@ GROUP BY u.name
 HAVING students_count>=8
 ORDER BY students_count DESC , university_name DESC;
 
+#9
+SELECT 
+    u.name AS university_name,
+    c.name AS city_name,
+    u.address,
+    CASE
+        WHEN u.tuition_fee < 800 THEN 'cheap'
+        WHEN u.tuition_fee < 1200 THEN 'normal'
+        WHEN u.tuition_fee < 2500 THEN 'high'
+        ELSE 'expensive'
+    END AS price_rank,
+    u.tuition_fee
+FROM
+    universities u
+        JOIN
+    cities c ON c.id = u.city_id
+ORDER BY u.tuition_fee;
+
+
