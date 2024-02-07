857d4c597b8fc3a93a3511697d8311ea3eb90d06
Donika <marchevadonika@gmail.com>
2024-02-07 18:17:12 +0200

    5

--- examPreparation/MySQL Retake Exam - 6 August 2023/solutions.sql
+++ examPreparation/MySQL Retake Exam - 6 August 2023/solutions.sql
@@ -97,6 +97,15 @@ WHERE
 DELETE FROM property_transactions 
 WHERE
     is_successful = 0;
+    
+#-----------------------------------------------
+
+#5
+SELECT 
+    id, first_name, last_name, phone, email, city_id
+FROM
+    agents
+ORDER BY city_id DESC , phone DESC;
 
 
 
