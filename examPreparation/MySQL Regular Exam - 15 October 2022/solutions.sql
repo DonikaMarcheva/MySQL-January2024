b6e2c3590dd11c9f3ed6c76ea2833cf7d75dd40a
Donika <marchevadonika@gmail.com>
2024-02-09 16:26:00 +0200

    7

--- examPreparation/MySQL Regular Exam - 15 October 2022/solutions.sql
+++ examPreparation/MySQL Regular Exam - 15 October 2022/solutions.sql
@@ -110,3 +110,17 @@ WHERE
 ORDER BY last_name DESC , id
 LIMIT 5;
 
+#7
+
+SELECT 
+    CONCAT(last_name,
+            first_name,
+            LENGTH(first_name),
+            'Restaurant') AS username,
+    REVERSE(SUBSTR(email, 2, 12)) AS password
+FROM
+    waiters
+WHERE
+    salary IS NOT NULL
+ORDER BY password DESC;
+
