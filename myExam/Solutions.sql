23b6dc596701ee31e5a63b2a1bb62ccff6333ff0
Donika <marchevadonika@gmail.com>
2024-02-12 10:13:22 +0200

    add all solutions for my exam-100/100

--- myExam/Solutions.sql
+++ myExam/Solutions.sql
@@ -1,61 +1,211 @@
 use preserves_db;
 
-#1
-
-CREATE TABLE continents(
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(40) NOT NULL UNIQUE
+CREATE TABLE continents (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL UNIQUE
 );
 
-CREATE TABLE countries(
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(40) NOT NULL UNIQUE,
-country_code VARCHAR(10) NOT NULL UNIQUE,
-continent_id INT NOT NULL,
-
-FOREIGN KEY (continent_id) REFERENCES continents(id)
+CREATE TABLE countries (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL UNIQUE,
+    country_code VARCHAR(10) NOT NULL UNIQUE,
+    continent_id INT NOT NULL,
+    FOREIGN KEY (continent_id)
+        REFERENCES continents (id)
 );
 
-CREATE TABLE preserves(
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(255) NOT NULL UNIQUE,
-latitude DECIMAL(9,6),
-longitude DECIMAL(9,6),
-area INT,
-type VARCHAR(20),
-established_on DATE
+CREATE TABLE preserves (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    latitude DECIMAL(9 , 6 ),
+    longitude DECIMAL(9 , 6 ),
+    area INT,
+    type VARCHAR(20),
+    established_on DATE
 );
 
-CREATE TABLE positions(
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(40) NOT NULL UNIQUE,
-description TEXT,
-is_dangerous BOOLEAN NOT NULL
+CREATE TABLE positions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL UNIQUE,
+    description TEXT,
+    is_dangerous BOOLEAN NOT NULL
 );
 
-CREATE TABLE workers(
-id INT PRIMARY KEY AUTO_INCREMENT,
-first_name VARCHAR(40) NOT NULL,
-last_name VARCHAR(40) NOT NULL,
-age INT,
-personal_number VARCHAR(20) NOT NULL UNIQUE,
-salary DECIMAL(19,2),
-is_armed BOOLEAN NOT NULL,
-start_date DATE,
-preserve_id INT,
-position_id INT,
-
-FOREIGN KEY (preserve_id) REFERENCES preserves(id),
-FOREIGN KEY (position_id) REFERENCES positions(id)
+CREATE TABLE workers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    age INT,
+    personal_number VARCHAR(20) NOT NULL UNIQUE,
+    salary DECIMAL(19 , 2 ),
+    is_armed BOOLEAN NOT NULL,
+    start_date DATE,
+    preserve_id INT,
+    position_id INT,
+    FOREIGN KEY (preserve_id)
+        REFERENCES preserves (id),
+    FOREIGN KEY (position_id)
+        REFERENCES positions (id)
 );
 
-CREATE TABLE countries_preserves(
-country_id INT NOT NULL,
-preserve_id INT NOT NULL,
-
-FOREIGN KEY (country_id) REFERENCES countries(id),
-FOREIGN KEY (preserve_id) REFERENCES preserves(id)
+CREATE TABLE countries_preserves (
+    country_id INT NOT NULL,
+    preserve_id INT NOT NULL,
+    FOREIGN KEY (country_id)
+        REFERENCES countries (id),
+    FOREIGN KEY (preserve_id)
+        REFERENCES preserves (id)
 );
 
 #2
+
+INSERT INTO preserves (name , latitude , longitude , area , type , established_on)
+SELECT 
+CONCAT(name, ' is in South Hemisphere'),
+latitude,
+longitude,
+area * id,
+LOWER(type),
+established_on
+FROM preserves
+WHERE latitude < 0;
+
+#3
+UPDATE workers 
+SET 
+    salary = salary + 500
+WHERE
+    position_id = 5 OR position_id = 8 OR position_id= 11 OR position_id=13; 
+    
+#4
+
+DELETE FROM preserves 
+WHERE
+   established_on IS NULL;
+    
+#-------------------------------------------------
+
+#5
+
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS full_name,
+    DATEDIFF('2024-01-01', start_date) AS days_of_experience
+FROM
+    workers
+WHERE
+    DATEDIFF('2024-01-01', start_date) / 365 > 5
+ORDER BY days_of_experience DESC
+LIMIT 10;
+
+#6
+
+SELECT 
+    w.id,
+    w.first_name,
+    w.last_name,
+    p.name AS preserve_name,
+    c.country_code
+FROM
+    workers w
+        JOIN
+    preserves p ON p.id = w.preserve_id
+        JOIN
+    countries_preserves cp ON cp.preserve_id = p.id
+        JOIN
+    countries c ON c.id = cp.country_id
+WHERE
+    w.salary > 5000 AND age < 50
+ORDER BY c.country_code;
+
+#7
+
+SELECT 
+    p.name, COUNT(w.id) AS armed_workers
+FROM
+    preserves p
+        JOIN
+    workers w ON w.preserve_id = p.id
+WHERE
+    w.is_armed = 1
+GROUP BY w.preserve_id
+ORDER BY armed_workers DESC , p.name;
+
+#8
+
+SELECT 
+    p.name, c.country_code, YEAR(p.established_on) AS founded_in
+FROM
+    preserves p
+        JOIN
+    countries_preserves cp ON cp.preserve_id = p.id
+        JOIN
+    countries c ON c.id = cp.country_id
+WHERE
+    MONTH(p.established_on) = '05'
+ORDER BY founded_in
+LIMIT 5;
+
+#9
+
+SELECT 
+    id,
+    name,
+    CASE
+        WHEN area <= 100 THEN 'very small'
+        WHEN area <= 1000 THEN 'small'
+        WHEN area <= 10000 THEN 'medium'
+        WHEN area <= 50000 THEN 'large'
+        ELSE 'very large'
+    END AS category
+FROM
+    preserves
+ORDER BY area DESC;
+
+#--------------------------------
+
+#10
+
+DELIMITER $
+
+CREATE FUNCTION udf_average_salary_by_position_name (name VARCHAR(40)) 
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN 
+RETURN 
+(SELECT AVG(w.salary) 
+FROM workers w
+JOIN positions p ON p.id=w.position_id
+WHERE p.name=name
+GROUP BY w.position_id);
+END$
+
+DELIMITER ;
+
+SELECT p.name, udf_average_salary_by_position_name('Forester') as position_average_salary FROM positions p 
+WHERE p.name = 'Forester'
+
+#11
+
+DELIMITER $
+CREATE PROCEDURE udp_increase_salaries_by_country (country_name VARCHAR(40))
+BEGIN
+UPDATE workers w
+	JOIN preserves p ON p.id = w.preserve_id
+	JOIN countries_preserves cp ON cp.preserve_id = p.id
+    JOIN countries c ON cp.country_id=c.id
+	SET w.salary = w.salary+0.05*w.salary
+	WHERE c.name = country_name;
+END$
+
+DELIMITER ;
+
+DROP PROCEDURE udp_increase_salaries_by_country;
+
+CALL udp_increase_salaries_by_country ('Germany');
+
+
+SELECT EXTRACT(DAY FROM '2000-02-03'); 
+SELECT DAY('2000-02-03');
+
+
 
